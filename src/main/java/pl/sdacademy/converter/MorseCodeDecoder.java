4242e784edf80d3f0d7827b8526a815e7078e531
tomas <tomaszkozak1996@gmail.com>
2023-03-21 19:58:16 +0100

    Task 1 - MorseCodeDictionary added

---
+++ src/main/java/pl/sdacademy/converter/MorseCodeDecoder.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeDecoder {
+
+    private final MorseCodeDictionary dictionary;
+
+    public MorseCodeDecoder(MorseCodeDictionary dictionary){
+        this.dictionary = dictionary;
+    }
+    public String decode(String morseCode){
+        return "";
+    }
+}
---
+++ src/main/java/pl/sdacademy/converter/MorseCodeEncoder.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.converter;
+
+public class MorseCodeEncoder {
+    private final MorseCodeDictionary dictionary;
+
+    public MorseCodeEncoder(MorseCodeDictionary dictionary){
+        this.dictionary = dictionary;
+    }
+
+    public String encode(String text){
+        return "";
+    }
+}
